87e843d72ce089bffba97fa07ec81cdca5b18815
angel <angel.cantu@moneybox.business>
2025-11-11 01:06:19 -0600

    se agrego dockercomposer y configuracion de factura electronica

--- config/app.php
+++ config/app.php
@@ -1,19 +1,31 @@
 <?php
 
-	const APP_URL="http://localhost/VENTAS/";
+	const APP_URL="__APPURL__";
 	const APP_NAME="VENTAS";
 	const APP_SESSION_NAME="POS";
+	// const PAIS="60"; // el salvador
+	const PAIS="__PAIS_CODE__"; // el salvador
 
 	/*----------  Tipos de documentos  ----------*/
-	const DOCUMENTOS_USUARIOS=["DUI","DNI","Cedula","Licencia","Pasaporte","Otro"];
+	if( !strcmp(PAIS, 60) ) {
+	 	define('DOCUMENTOS_USUARIOS', ["DUI","DNI","Cedula","Licencia","Pasaporte","Otro"]);
+	}
+	else {
+		define('DOCUMENTOS_USUARIOS', ["INE","RFC","CURP","Licencia","Pasaporte","Otro"]);
+	}
 
 
 	/*----------  Tipos de unidades de productos  ----------*/
-	const PRODUCTO_UNIDAD=["Unidad","Libra","Kilogramo","Caja","Paquete","Lata","Galon","Botella","Tira","Sobre","Bolsa","Saco","Tarjeta","Otro"];
+	const PRODUCTO_UNIDAD=["Pieza", "Unidad","Libra","Kilogramo","Caja","Paquete","Lata","Galon","Botella","Tira","Sobre","Bolsa","Saco","Tarjeta","Otro"];
 
 	/*----------  Configuración de moneda  ----------*/
 	const MONEDA_SIMBOLO="$";
-	const MONEDA_NOMBRE="USD";
+
+	if( !strcmp(PAIS, 60) )
+		define('MONEDA_NOMBRE', "USD");
+	else
+		define('MONEDA_NOMBRE', "MXN");
+
 	const MONEDA_DECIMALES="2";
 	const MONEDA_SEPARADOR_MILLAR=",";
 	const MONEDA_SEPARADOR_DECIMAL=".";
@@ -23,7 +35,10 @@
 	const CAMPO_OBLIGATORIO='&nbsp; <i class="fas fa-edit"></i> &nbsp;';
 
 	/*----------  Zona horaria  ----------*/
-	date_default_timezone_set("America/El_Salvador");
+	if( !strcmp(PAIS, 60) )
+		date_default_timezone_set("America/El_Salvador");
+	else
+		date_default_timezone_set("America/Matamoros");
 
 	/*
 		Configuración de zona horaria de tu país, para más información visita
