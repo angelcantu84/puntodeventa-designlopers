126e8771b0d56f07df7c3037f892c64e44563a4b
angel <angel.cantu@moneybox.business>
2025-11-11 10:51:30 -0600

    se mejoraron expresiones regulares de telefono, se agrego selector de paises para tener multiregional la plataforma, los clientes ya se pueden crear y editar respetando la nueva info

--- config/app.php
+++ config/app.php
@@ -7,11 +7,20 @@
 	const PAIS="__PAIS_CODE__"; // el salvador
 
 	/*----------  Tipos de documentos  ----------*/
-	if( !strcmp(PAIS, 60) ) {
-	 	define('DOCUMENTOS_USUARIOS', ["DUI","DNI","Cedula","Licencia","Pasaporte","Otro"]);
+	if( !strcmp(PAIS, 60) ) { // el salvador
+	 	define('DOCUMENTOS_USUARIOS', ["NIT","DUI","Cedula","Licencia","Pasaporte","Otro"]);
 	}
-	else {
-		define('DOCUMENTOS_USUARIOS', ["INE","RFC","CURP","Licencia","Pasaporte","Otro"]);
+	else if( !strcmp(PAIS, 47) ) { // colombia
+	 	define('DOCUMENTOS_USUARIOS', ["NIT","Cedula","Extranjeria","Pasaporte","Otro"]);
+	}
+	else if( !strcmp(PAIS, 174) ) { // panama
+	 	define('DOCUMENTOS_USUARIOS', ["RUC","Cedula","Extranjeria","Pasaporte","Otro"]);
+	}
+	else if( !strcmp(PAIS, 188) ) { // rep dom
+	 	define('DOCUMENTOS_USUARIOS', ["RNC","Cedula","Extranjeria","Pasaporte","Otro"]);
+	}
+	else { // mexico
+		define('DOCUMENTOS_USUARIOS', ["RFC", "INE","CURP","Pasaporte","Otro"]);
 	}
 
 
@@ -21,9 +30,15 @@
 	/*----------  Configuración de moneda  ----------*/
 	const MONEDA_SIMBOLO="$";
 
-	if( !strcmp(PAIS, 60) )
+	if( !strcmp(PAIS, 60) ) // el salvador
 		define('MONEDA_NOMBRE', "USD");
-	else
+	else if( !strcmp(PAIS, 47) ) // colombia
+		define('MONEDA_NOMBRE', "COP");
+	else if( !strcmp(PAIS, 174) ) // panama
+		define('MONEDA_NOMBRE', "PAB");
+	else if( !strcmp(PAIS, 188) ) // rep dom
+		define('MONEDA_NOMBRE', "DOP");
+	else // mexico
 		define('MONEDA_NOMBRE', "MXN");
 
 	const MONEDA_DECIMALES="2";
@@ -35,8 +50,14 @@
 	const CAMPO_OBLIGATORIO='&nbsp; <i class="fas fa-edit"></i> &nbsp;';
 
 	/*----------  Zona horaria  ----------*/
-	if( !strcmp(PAIS, 60) )
+	if( !strcmp(PAIS, 60) ) // el salvador
 		date_default_timezone_set("America/El_Salvador");
+	else if( !strcmp(PAIS, 47) ) // colombia
+		date_default_timezone_set("America/Bogota");
+	else if( !strcmp(PAIS, 174) ) // panama
+		date_default_timezone_set("America/Panama");
+	else if( !strcmp(PAIS, 188) ) // rep dom
+		date_default_timezone_set("America/Santo_Domingo");
 	else
 		date_default_timezone_set("America/Matamoros");
 
